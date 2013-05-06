8b6df43e08398e5e27193e0c0fe627f8f2d22b38
lihaoyi <haoyi.sg@gmail.com>
2013-05-06 17:25:50 -0400

    demo stuff ready

--- macropy/demo.py
+++ macropy/demo.py
@@ -79,6 +79,52 @@ pattern matching
 
 peg
 -------
+from macropy.macros.quicklambda import macros, f
+from macropy.macros2.peg import macros
+from macropy.macros2.peg import *
+with peg:
+    value = '[0-9]+'.r | ('(', expr, ')')
+    op = '+' | '-' | '*' | '/'
+    expr = (value, (op, value).rep )
+
+print expr.parse_all("123")
+print expr.parse_all("123boo")
+print expr.parse_all("((123))")
+print expr.parse_all("((123)")
+print expr.parse_all("(123+456+789)")
+print expr.parse_all("(6/2)")
+print expr.parse_all("(1+2+3)+2")
+print expr.parse_all("(((((((11)))))+22+33)*(4+5+((6))))/12*(17+5)")
+
+
+from macropy.macros.quicklambda import macros, f
+from macropy.macros2.peg import macros
+from macropy.macros2.peg import *
+
+def reduce_chain(chain):
+    chain = list(reversed(chain))
+    o_dict = {
+        "+": f%(_+_),
+        "-": f%(_-_),
+        "*": f%(_*_),
+        "/": f%(_/_),
+    }
+    while len(chain) > 1:
+        a, [o, b] = chain.pop(), chain.pop()
+        chain.append(o_dict[o](a, b))
+    return chain[0]
+
+with peg:
+    value = '[0-9]+'.r // int | ('(', expr, ')') // (f%_[1])
+    op = '+' | '-' | '*' | '/'
+    expr = (value is first, (op, value).rep is rest) >> reduce_chain([first] + rest)
+
+print expr.parse_all("123")             #[123]
+print expr.parse_all("((123))")         #[123]
+print expr.parse_all("(123+456+789)")   #[1368]
+print expr.parse_all("(6/2)")           #[3]
+print expr.parse_all("(1+2+3)+2")       #[8]
+print expr.parse_all("(((((((11)))))+22+33)*(4+5+((6))))/12*(17+5)") #[1804]
 
 """
 
