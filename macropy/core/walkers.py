fc701028501192216726563abcfd6983e2a76695
lihaoyi <haoyi.sg@gmail.com>
2013-05-11 10:24:02 -0400

    comments for walker.py

--- macropy/core/walkers.py
+++ macropy/core/walkers.py
@@ -7,6 +7,25 @@ stop = object()
 
 
 class GenericWalker(object):
+    """
+    Decorates a function of the form
+
+    @GenericWalker
+    def transform(tree, ctx):
+        ...
+        return new_tree, new_ctx, aggregate
+
+    new_tree, all_aggregates = transform.recurse(old_tree, initial_ctx)
+
+    Where `aggregate` is a `list` of 0 or more elements, which will be
+    aggregated into one final list (`all_aggregates`) when recurse returns.
+    `new_ctx` is an arbitrary value which is passed down from a tree to its
+    children automatically by the recursion, who will receive it in the `ctx`
+    parameter when they are recursed upon.
+
+    The decorated function can also terminate the recursion by returning the
+    singleton value `stop` as `new_ctx`.
+    """
     def __init__(self, func):
         self.func = func
         #self.flatten = lambda mylist: [l for sub in mylist for l in sub]
@@ -17,7 +36,7 @@ class GenericWalker(object):
             for field, old_value in iter_fields(tree):
                 old_value = getattr(tree, field, None)
                 new_value, new_aggregate = self.recurse_real(old_value, ctx)
-                aggregates.append(new_aggregate)
+                aggregates.extend(new_aggregate)
 
                 setattr(tree, field, new_value)
             return aggregates
@@ -25,7 +44,7 @@ class GenericWalker(object):
             x = zip(*map(lambda x: self.recurse_real(x, ctx), tree))
             [trees, aggregates] = x
             tree[:] = flatten(trees)
-            return flatten(aggregates)
+            return [x for y in aggregates for x in y]
         else:
             return []
 
@@ -39,13 +58,28 @@ class GenericWalker(object):
             x = self.func(tree, ctx)
             if x is not None :
                 tree, new_ctx, aggregate = x
+                assert type(aggregate) is list
                 aggregates = self.walk_children(tree, new_ctx)
-                return tree, flatten([aggregate] + aggregates)
+
+                return tree, aggregate + aggregates
             else:
                 return tree, []
 
 
 class AggregateWalker(GenericWalker):
+    """
+    Decorates a function of the form
+
+    @AggregateWalker
+    def transform(tree):
+        ...
+        return new_tree, aggregate
+
+    new_tree, all_aggregates = transform.recurse(old_tree)
+
+    Where `aggregate` is a `list` of 0 or more elements, which will be
+    aggregated into one final list (`all_aggregates`) when recurse returns.
+    """
     def __init__(self, func):
         self.func = lambda tree, ctx: (lambda x: (x[0], [], x[1]))(func(tree))
 
@@ -54,6 +88,24 @@ class AggregateWalker(GenericWalker):
 
 
 class ContextWalker(GenericWalker):
+    """
+    Decorates a function of the form
+
+    @ContextWalker
+    def transform(tree, ctx):
+        ...
+        return new_tree, new_ctx
+
+    new_tree = transform.recurse(old_tree, initial_context)
+
+    Where `new_ctx` is an arbitrary value which is passed down from a tree to its
+    children automatically by the recursion, who will receive it in the `ctx`
+    parameter when they are recursed upon.
+
+    The decorated function can also terminate the recursion by returning the
+    singleton value `stop` as `new_ctx`.
+
+    """
     def __init__(self, func):
         self.func = lambda tree, ctx: (lambda x: (x[0], x[1], []))(func(tree, ctx))
 
@@ -62,6 +114,16 @@ class ContextWalker(GenericWalker):
 
 
 class Walker(GenericWalker):
+    """
+    Decorates a function of the form
+
+    @Walker
+    def transform(tree):
+        ...
+        return new_tree
+
+    new_tree = transform.recurse(old_tree)
+    """
     def __init__(self, func):
         self.func = lambda tree, ctx: (func(tree), [], [])
 
