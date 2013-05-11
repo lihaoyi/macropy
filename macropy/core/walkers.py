fff89cef355d6f76cf7c8b7d7e4dc81cd15875b4
lihaoyi <haoyi.sg@gmail.com>
2013-05-11 10:13:19 -0400

    Considerably cleaned up `walk_children` and `recurse_real`

--- macropy/core/walkers.py
+++ macropy/core/walkers.py
@@ -9,42 +9,40 @@ stop = object()
 class GenericWalker(object):
     def __init__(self, func):
         self.func = func
+        #self.flatten = lambda mylist: [l for sub in mylist for l in sub]
 
     def walk_children(self, tree, ctx=None):
-        aggregates = []
-        for field, old_value in list(iter_fields(tree)):
-            old_value = getattr(tree, field, None)
-            new_value, new_aggregate = self.recurse_real(old_value, ctx)
-            aggregates.append(new_aggregate)
+        if isinstance(tree, AST):
+            aggregates = []
+            for field, old_value in iter_fields(tree):
+                old_value = getattr(tree, field, None)
+                new_value, new_aggregate = self.recurse_real(old_value, ctx)
+                aggregates.append(new_aggregate)
 
-            setattr(tree, field, new_value)
-        return aggregates
+                setattr(tree, field, new_value)
+            return aggregates
+        elif isinstance(tree, list) and len(tree) > 0:
+            x = zip(*map(lambda x: self.recurse_real(x, ctx), tree))
+            [trees, aggregates] = x
+            tree[:] = flatten(trees)
+            return flatten(aggregates)
+        else:
+            return []
 
     def recurse(self, tree, ctx=None):
         return self.recurse_real(tree, ctx)
 
     def recurse_real(self, tree, ctx=None):
-        if isinstance(tree, list) and len(tree) > 0:
-            x = zip(*map(lambda x: self.recurse_real(x, ctx), tree))
-            [trees, aggregates] = x
-            return flatten(list(trees)), flatten(aggregates)
-
-        elif isinstance(tree, comprehension):
-            aggregates = self.walk_children(tree, ctx)
-            return tree, flatten(aggregates)
-        elif isinstance(tree, AST):
-            tree, new_ctx, aggregate = self.func(tree, ctx)
-            if new_ctx is not stop:
-                if type(tree) is list:
-                    tree, aggregate2 = self.recurse_real(tree, new_ctx)
-                    return tree, flatten(aggregate + aggregate2)
-                else:
-                    aggregates = self.walk_children(tree, new_ctx)
-                    return tree, flatten([aggregate] + aggregates)
-            else:
-                return tree, aggregate
-        else:
+        if ctx is stop:
             return tree, []
+        else:
+            x = self.func(tree, ctx)
+            if x is not None :
+                tree, new_ctx, aggregate = x
+                aggregates = self.walk_children(tree, new_ctx)
+                return tree, flatten([aggregate] + aggregates)
+            else:
+                return tree, []
 
 
 class AggregateWalker(GenericWalker):
