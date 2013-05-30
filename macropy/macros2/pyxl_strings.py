0f09291334514afa9b3092993c5ffa760a7b35d4
lihaoyi <haoyi.sg@gmail.com>
2013-05-29 23:02:08 -0400

    fixed a minor bug in pyxl/Pypy

--- macropy/macros2/pyxl_strings.py
+++ macropy/macros2/pyxl_strings.py
@@ -13,6 +13,7 @@ macros = Macros()
 @macros.expr()
 def p(tree, **kw):
     import StringIO
-    new_string = tokenize.untokenize(pyxl_tokenize(StringIO.StringIO('(' + tree.s + ')').readline)).rstrip()
-    new_tree = ast.parse("from __future__ import unicode_literals;" + new_string)
+    new_string = tokenize.untokenize(pyxl_tokenize(StringIO.StringIO('(' + tree.s + ')').readline)).rstrip().rstrip("\\")
+    total_string = "from __future__ import unicode_literals;" + new_string
+    new_tree = ast.parse(total_string)
     return new_tree.body[1].value
