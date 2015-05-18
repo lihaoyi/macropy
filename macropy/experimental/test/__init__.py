c4b829174dc342c82f4fda0143149cca7deed34c
Ceridwen <ceridwenv@gmail.com>
2015-05-18 17:35:39 -0400

    Comment out pyxl tests

--- macropy/experimental/test/__init__.py
+++ macropy/experimental/test/__init__.py
@@ -2,13 +2,13 @@ from macropy.test import test_suite
 #import js_snippets
 #import pattern
 #import pinq
-import pyxl_snippets
+# import pyxl_snippets
 #import tco_test
 
 Tests = test_suite(cases = [
     #js_snippets,
     #pattern,
     #pinq,
-    pyxl_snippets,
+    # pyxl_snippets,
     #tco_test
 ])
