61dd69718fd1212c8ae4238c78f17a32c5481193
rebcabin <bbeckman@amazon.com>
2015-10-20 18:59:46 -0700

    add dumpid macro
    A macro with semantics of the identity function that prints its
    expression and value as a side effect.

--- macropy/test/dump.py
+++ macropy/test/dump.py
@@ -1,7 +1,8 @@
 import unittest
 
-from macropy.dump import macros, dump
+from macropy.dump import macros, dump, dumpid
 class Tests(unittest.TestCase):
+
     def test_basic(self):
         assert dump[1] == '1 ~~> 1'
         assert dump[1 + 2] == '(1 + 2) ~~> 3'
@@ -14,3 +15,16 @@ class Tests(unittest.TestCase):
     def test_lambda(self):
         assert dump[(lambda x: x * 3 + 3 * x)(7)] == \
             '(lambda x: ((x * 3) + (3 * x)))(7) ~~> 42'
+
+    def test_basic_id(self):
+        assert dumpid[1] == 1
+        assert dumpid[1 + 2] == 3
+        assert dumpid[2 + 1] == 3
+        assert dumpid[3*7  +  7*3] == 42
+
+    def test_if(self):
+        assert dumpid[42 if None else 43] == 43
+
+    def test_lambda(self):
+        assert dumpid[(lambda x: x * 3 + 3 * x)(7)] == 42
+
