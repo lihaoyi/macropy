dfd2d023ab3f5cfaa53814c299e01ea5c577852c
Alberto Berti <alberto@metapensiero.it>
2017-04-10 00:15:32 +0200

    Add a flag for PY33 and an xrange function

--- macropy/core/compat.py
+++ macropy/core/compat.py
@@ -3,10 +3,13 @@
 import sys
 
 PY3 = sys.version_info >= (3,)
+PY33 = sys.version_info >= (3, 3)
 PY34 = sys.version_info >= (3, 4)
 PY35 = sys.version_info >= (3, 5)
 
 if PY3:
     string_types = (str,)
+    xrange = __builtins__['range']
 else:
     string_types = (str, unicode)
+    xrange = __builtins__['xrange']
