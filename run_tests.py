e411c995dcd52315201101b738e719314118b925
Omer Katz <omer.drow@gmail.com>
2017-06-13 12:34:31 +0300

    Exit with return code 1 when tests fail.

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,11 @@
+import sys
 import unittest
 import macropy.activate
 
 import macropy.test
 
-unittest.TextTestRunner().run(macropy.test.Tests)
+result = unittest.TextTestRunner().run(macropy.test.Tests)
+sys.exit(1 if result.errors or result.failures else 0)
 
 
 
