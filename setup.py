1045b5fbd77cdf6e0c17e014a3c763c09e8def4e
Alberto Berti <alberto@metapensiero.it>
2017-09-08 18:20:48 +0200

    Update setup script for release
    - add some notes to the long description similar to those on top of
      the readme.rst;
    
    - change the name of the package so that it doesn't clash with Py2
      macropy;
    
    - add changes to the long description.

--- setup.py
+++ setup.py
@@ -1,17 +1,36 @@
-"""MacroPy is an implementation of Macros in the Python Programming
+# -*- coding: utf-8; mode: python -*-
+"""
+.. image:: https://travis-ci.org/azazel75/macropy.svg?branch=master
+  :target: https://travis-ci.org/azazel75/macropy
+
+.. warning::
+
+   This is a customized version of the original `MacroPy`__
+   updated to work only with Python 3.5+. PyPy is untested.
+
+   As of now the original tests pass but some language features have
+   yet to be implemented.
+
+   What follows is an ongoing update of the original documentation and
+   therefore may be inaccurate.
+
+__ https://github.com/lihaoyi/macropy
+
+MacroPy is an implementation of Macros in the Python Programming
 Language.  MacroPy provides a mechanism for user-defined functions
 (macros) to perform transformations on the abstract syntax tree(AST)
 of Python code at module import time. This is an easy way to modify
 the semantics of a python program
 
 Python like you've never seen before
-------------------------------------
+====================================
 
 MacroPy allows you to create constructs which are impossible to have
 in normal python code, such as:
 
 Tracing
-```````
+-------
+
 .. code:: python
 
     with trace:
@@ -26,7 +45,7 @@ Tracing
     # sum([x + 5 for x in range(3)]) -> 18
 
 Quick Lambdas
-`````````````
+-------------
 
 .. code:: python
 
@@ -37,7 +56,7 @@ Quick Lambdas
     # 'omgwtfbbq
 
 Case Classes
-````````````
+------------
 
 .. code:: python
 
@@ -58,19 +77,25 @@ Group <https://groups.google.com/forum/#!forum/macropy>`_.
 
 """
 
-
+from pathlib import Path
 from setuptools import find_packages, setup
-from macropy import __version__
+from . import __version__
+
+here = Path(__file__).absolute().parent
+with (here / 'CHANGES.rst').open(encoding='utf-8') as f:
+    CHANGES = f.read()
 
 setup(
-    name='MacroPy',
+    name='macropy3',
     version=__version__,
     description='Macros for Python: Quasiquotes, Case Classes, LINQ and more!',
-    long_description=__doc__,
+    long_description=__doc__ + CHANGES,
     license='MIT',
     author='Li Haoyi, Justin Holmgren',
     author_email='haoyi.sg@gmail.com, justin.holmgren@gmail.com',
-    url='https://github.com/lihaoyi/macropy',
+    maintainer='Alberto Berti',
+    maintainer_email='alberto@metapensiero.it',
+    url='https://github.com/azazel75/macropy',
     packages=find_packages(exclude=["*.test", "*.test.*"]),
     extras_require={
         'pyxl':  ["pyxl"],
