cae45ab21cee7ccc1d32a6b3da084f59bf4e0578
Alberto Berti <alberto@metapensiero.it>
2018-03-02 00:04:55 +0100

    Remove Py2 compatibility code

--- macropy/core/compat.py
+++ macropy/core/compat.py
@@ -3,18 +3,10 @@
 import ast
 import sys
 
-PY3 = sys.version_info >= (3,)
 PY33 = sys.version_info >= (3, 3)
 PY34 = sys.version_info >= (3, 4)
 PY35 = sys.version_info >= (3, 5)
 PY36 = sys.version_info >= (3, 6)
-
-if PY3:
-    string_types = (str,)
-    xrange = __builtins__['range']
-else:
-    string_types = (str, unicode)
-    xrange = __builtins__['xrange']
 
 if PY34:
     function_nodes = (ast.FunctionDef,)
