6b9903f758492ba753b410950a358f027ed6b154
Alberto Berti <alberto@metapensiero.it>
2018-04-11 04:40:45 +0200

    Port tco to Py3.5+ and enable its tests

--- macropy/experimental/tco.py
+++ macropy/experimental/tco.py
@@ -1,27 +1,25 @@
 import ast
+import enum
+import functools
 
-import macropy.core.macros
-import macropy.core.util
-import macropy.core.walkers
+from ..core import Captured  # noqa: F401
+from ..core.hquotes import macros, hq  # noqa: F811
+from ..core.macros import Macros
+from ..core.walkers import Walker
+from ..core.compat import PY35
 
-from macropy.core import Captured
-from macropy.experimental.pattern import macros, switch, _matching, ClassMatcher, NameMatcher
+if not PY35:
+    raise RuntimeError("Tail-call optimization works only on Py3.5+ for now.")
 
-from macropy.core.hquotes import macros, hq
+from .pattern import (  # noqa: F401,F811
+    macros, switch, ClassMatcher, NameMatcher)
 
-__all__ = ['tco']
 
-macros = macropy.core.macros.Macros()
+macros = Macros()  # noqa: F811
 
 in_tc_stack = [False]
 
-@macropy.core.util.singleton
-class TcoIgnore:
-    pass
-
-@macropy.core.util.singleton
-class TcoCall:
-    pass
+TCOType = enum.Enum('TCOType', ('IGNORE', 'CALL'))
 
 
 def trampoline(func, args, kwargs):
@@ -31,6 +29,7 @@ def trampoline(func, args, kwargs):
     The function may return (tco.CALL, func, args, kwargs) or (tco.IGNORE,
     func, args, kwargs) or just a value.
     """
+
     ignoring = False
     while True:
         # We can only set this if we know it will be immediately unset by func
@@ -39,12 +38,12 @@ def trampoline(func, args, kwargs):
         result = func(*args, **kwargs)
         # for performance reasons, do not use pattern matching here
         if isinstance(result, tuple):
-            if result[0] is TcoCall:
+            if result[0] is TCOType.CALL:
                 func = result[1]
                 args = result[2]
                 kwargs = result[3]
                 continue
-            elif result[0] is TcoIgnore:
+            elif result[0] is TCOType.IGNORE:
                 ignoring = True
                 func = result[1]
                 args = result[2]
@@ -56,9 +55,8 @@ def trampoline(func, args, kwargs):
             return result
 
 
-
 def trampoline_decorator(func):
-    import functools
+
     @functools.wraps(func)
     def trampolined(*args, **kwargs):
         if in_tc_stack[0]:
@@ -74,25 +72,31 @@ def trampoline_decorator(func):
 @macros.decorator
 def tco(tree, **kw):
 
-    @macropy.core.walkers.Walker
+    @Walker
     # Replace returns of calls
     def return_replacer(tree, **kw):
         with switch(tree):
             if ast.Return(value=ast.Call(
                     func=func,
                     args=args,
-                    starargs=starargs,
-                    kwargs=kwargs)):
-                if starargs:
+                    keywords=keywords)):
+                starred = [arg for arg in args if isinstance(arg, ast.Starred)]
+                kwargs = [kw for kw in keywords if kw.arg is None]
+
+                if len(kwargs):
+                    kwargs = kwargs[0].value
+                if len(starred):
+                    starred = starred[0].value
                     with hq as code:
                     # get rid of starargs
-                        return (TcoCall,
+                        return (TCOType.CALL,
                                 ast_literal[func],
-                                ast_literal[ast.List(args, ast.Load())] + list(ast_literal[starargs]),
+                                (ast_literal[ast.List(args, ast.Load())] +
+                                 list(ast_literal[starred])),
                                 ast_literal[kwargs or ast.Dict([],[])])
                 else:
                     with hq as code:
-                        return (TcoCall,
+                        return (TCOType.CALL,
                                 ast_literal[func],
                                 ast_literal[ast.List(args, ast.Load())],
                                 ast_literal[kwargs or ast.Dict([], [])])
@@ -103,27 +107,29 @@ def tco(tree, **kw):
 
     # Replace calls (that aren't returned) which happen to be in a tail-call
     # position
-
-    # WARN!!: This has to be fixed to support both Py3.4 and 3.5+, do
-    # not use, the problem is that ast.Call changed significantly
-    # between Py3.4 and 3.5
     def replace_tc_pos(node):
         with switch(node):
             if ast.Expr(value=ast.Call(
                     func=func,
                     args=args,
-                    starargs=starargs,
-                    kwargs=kwargs)):
-                if starargs:
+                    keywords=keywords)):
+                starred = [arg for arg in args if isinstance(arg, ast.Starred)]
+                kwargs = [kw for kw in keywords if kw.arg is None]
+
+                if len(kwargs):
+                    kwargs = kwargs[0].value
+                if len(starred):
+                    starred = starred[0].value
                     with hq as code:
                     # get rid of starargs
-                        return (TcoIgnore,
+                        return (TCOType.IGNORE,
                                 ast_literal[func],
-                                ast_literal[ast.List(args, ast.Load())] + list(ast_literal[starargs]),
+                                (ast_literal[ast.List(args, ast.Load())] +
+                                 list(ast_literal[starred])),
                                 ast_literal[kwargs or ast.Dict([],[])])
                 else:
                     with hq as code:
-                        return (TcoIgnore,
+                        return (TCOType.IGNORE,
                                 ast_literal[func],
                                 ast_literal[ast.List(args, ast.Load())],
                                 ast_literal[kwargs or ast.Dict([], [])])
@@ -139,8 +145,7 @@ def tco(tree, **kw):
     tree = return_replacer.recurse(tree)
 
     tree.decorator_list = ([hq[trampoline_decorator]] +
-            tree.decorator_list)
+                           tree.decorator_list)
 
     tree.body[-1] = replace_tc_pos(tree.body[-1])
-
     return tree
--- macropy/experimental/test/__init__.py
+++ macropy/experimental/test/__init__.py
@@ -8,6 +8,7 @@ from . import pattern
 # import pinq
 # import pyxl_snippets
 # import tco
+from macropy.core.compat import PY35
 
 
 cases = [pattern]
@@ -19,5 +20,10 @@ try:
 except ImportError:
     print('Excluding pinq tests')
 
+if PY35:
+    from . import tco
+    cases.append(tco)
+else:
+    print('Exluding tco tests')
 
 Tests = test_suite(cases = cases)
