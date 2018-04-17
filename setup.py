9b1a3eb4913876d3ca7c4887de6de973abe25d26
Alberto Berti <alberto@metapensiero.it>
2018-04-18 00:44:14 +0200

    Add more classifiers

--- setup.py
+++ setup.py
@@ -107,8 +107,14 @@ setup(
     },
     install_requires=[],
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ]
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Code Generators',
+    ],
 )
